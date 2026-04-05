d93a6cf9b8191689629da5da168ac47be82abe99
Andre <andrefernandes3@gmail.com>
2026-04-05 08:00:48 -0300

    AJUSTE

--- api/GerenciarLista/index.js
+++ api/GerenciarLista/index.js
@@ -4,26 +4,30 @@ const client = new MongoClient(uri);
 
 module.exports = async function (context, req) {
     const metodo = req.method;
+    await client.connect();
+    const db = client.db('app_compras');
+    const colecao = db.collection('lista_compras');
 
-    try {
-        await client.connect();
-        const db = client.db('app_compras');
-        const colecao = db.collection('lista_compras');
-
-        if (metodo === 'GET') {
-            const lista = await colecao.find({ comprado: false }).toArray();
-            context.res = { status: 200, body: lista };
-        } 
-        else if (metodo === 'POST') {
-            const { nome } = req.body;
-            await colecao.updateOne(
-                { item_nome: nome.toUpperCase() },
-                { $set: { comprado: false, data_adicao: new Date() } },
-                { upsert: true }
-            );
-            context.res = { status: 201, body: "Item adicionado!" };
-        }
-    } catch (error) {
-        context.res = { status: 500, body: error.message };
+    if (metodo === 'GET') {
+        // Busca apenas o que NÃO foi comprado
+        const lista = await colecao.find({ comprado: false }).toArray();
+        context.res = { status: 200, body: lista };
+    } 
+    else if (metodo === 'POST') {
+        const { nome } = req.body;
+        await colecao.updateOne(
+            { item_nome: nome.toUpperCase() },
+            { $set: { comprado: false, data_adicao: new Date() } },
+            { upsert: true }
+        );
+        context.res = { status: 201, body: "Item adicionado!" };
+    }
+    else if (metodo === 'PATCH') {
+        const { nome, comprado } = req.body;
+        await colecao.updateOne(
+            { item_nome: nome.toUpperCase() },
+            { $set: { comprado: comprado } }
+        );
+        context.res = { status: 200, body: "Atualizado!" };
     }
 };
